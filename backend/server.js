7b8f262bc26ea7d5645c1702a839a877782048a8
Leatile Mone <leatilemone@gmail.com>
2021-09-04 12:03:27 +0200

    heroku

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 });
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static('/frontend/build'));
 }
 
 
