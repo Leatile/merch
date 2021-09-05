427d0010bf59b8e3ad5332386cb81ae28ef4b439
Leatile Mone <leatilemone@gmail.com>
2021-09-05 23:27:04 +0200

    update server

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,10 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) =>{
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-//app.get('/',(req, res) => {
- //   res.send('Server is ready');
-//});
+
+app.get('/',(req, res) => {
+   res.send('Server is ready');
+});
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
@@ -36,10 +37,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 const port = process.env.PORT || 5000;
 
-httpServer.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
-});
 
-//app.listen(port, () => {
- //   console.log(`Server at http://localhost:${port}`);
-//});
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
